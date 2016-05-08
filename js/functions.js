4232f0d5d3eec4aaf3626fd81a6156514f8488b7
Mehdi Al Ouafi <alouafi.mehdi@gmail.com>
2016-05-08 16:34:18 +0200

    je travail sur la synchronisation des évènements

--- js/functions.js
+++ js/functions.js
@@ -5,12 +5,16 @@ $(function(){
       $listItems          = $('li'),
       $listItemsContainer = $('ul'),
       $albumCove          = $('#cover'),
-      tl                 = new TimelineMax();
+      $track              = $('.track'),
+      $listItemsTop       = $('.list-items-top'),
+      tl                  = new TimelineMax();
+      $playButton.click(function(){
+        tl.staggerTo($listItems,0.6,{y:"670%",ease: Expo.easeInOut},-0.1)
+        tl.to($albumCove,0.5,{borderRadius: "50%", height: 200, width: 200,left: "50%",x: "-50%",top:50,ease: Power1.easeOut},"-=0.8")
+        tl.to($titleSong,0.7, {x:80,y:-140,ease: Power2.easeInOut},"-=1")
+        tl.to($track,0.3,{opacity: 0},"-=0.7")
+        tl.to($albumCove,3,{rotation:360, ease: Power0.easeOut,repeat:-1},"-=0.5")
+        tl.to($listItemsTop, 0.3,{opacity: 0},0.4)
 
-      // $playButton.click(function(){
-        tl.staggerTo($listItems,0.7,{y:"670%",ease: Expo.easeIn},-0.1)
-        tl.to($albumCove,0.4,{borderRadius: "50%", height: 200, width: 200,left: "50%",x: "-50%",top:50})
-        tl.to($titleSong,0.3, {x:70,y:-140})
-
-      // });
+  });
 })
